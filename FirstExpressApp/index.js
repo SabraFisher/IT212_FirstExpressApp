7aac68138ace2173978b7cdddc6f67f246ef165a
Sabra Fisher <labcoatkitty@gmail.com>
2025-10-02 20:32:14 -0700

    crud

--- FirstExpressApp/index.js
+++ FirstExpressApp/index.js
@@ -1,10 +1,54 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3000
 
+let users = [
+    { name: "Tim", age: 20 },
+    { name: "Alice", age: 24 },
+    { name: "Lilly", age: 13 }
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Centralia!')
 })
+//create Specification HTTP POST method to create a resource
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    users.push(req.body);
+    res.status(201);
+    res.json(req.body);
+});
+
+
+//read all at once
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//     OR
+// read each by id
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id < 0 || id >= users.length) {
+        return res.status(404).send('User not found');
+    }
+    console.log("user requested id:", id);
+    res.json(users[0]);
+})
+
+//update  Specification HTTP POST method to update by replacing with data provided in the request body
+app.post('/users/:id', (req, res) => {
+    res.status(500);
+    res.send();
+});
+
+//delete   HTTP DELETE specification method
+app.delete();
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
