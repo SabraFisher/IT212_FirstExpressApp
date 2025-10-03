aae01952a2cf2da0c63391aa563857f2501b36c3
Sabra Fisher <labcoatkitty@gmail.com>
2025-10-02 21:41:06 -0700

    end of week save

--- FirstExpressApp/index.js
+++ FirstExpressApp/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const port = 3000;
 
 let users = [
     { id: crypto.randomUUID(), name: "Tim", age: 20 },
@@ -17,9 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello Centralia!')
 })
+
+
 //create Specification HTTP POST method to create a resource
 app.post('/users', (req, res) => {
     console.log(req.body);
+    //is this valid data? if not exit with 400 bad request
+    let user = req.body;
+    user.id = crypto.randomUUID();
     users.push(req.body);
     res.status(201);
     res.json(req.body);
@@ -35,11 +41,8 @@ app.get('/users', (req, res) => {
 // read each by id
 app.get('/users/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    if (id < 0 || id >= users.length) {
-        return res.status(404).send('User not found');
-    }
     console.log("user requested id:", id);
-    res.json(users[0]);
+    res.json(users.filter(u => u.id == req.params.id)[0]);
 })
 
 //update  Specification HTTP POST method to update by replacing with data provided in the request body
